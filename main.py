dcd260b410b94274e2e9087c6881f7e24ad0262c
Usernameorsmth <simidarawilliams@gmial.com>
2025-04-26 19:00:44 +0100

    Des

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 a = 10
 b = 12
-c = 0
+c = 12
 
-if a and b and c:
-    print("All the numbers have boolean value as True")
-else:
-    print("Atleast one number has boolean value as False")
+print(a != b)
+print(b != c)
 
-a = 10
-b = -10
-c = 0
+a = "python"
+b = "coding"
 
-if a > 0 or b > 0:
-    print("Either of the number is greater than 0")
-else:
-    print("No number is greater than 0")
+if a != b:
+    print(a, 'and', b, 'are different.')
 
-if b > 0 or c > 0:
-    print("Either of the number is greater than 0")
-else:
-    print("No number is greater than 0")
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print('Hello')
+
+
+
+
+
+
+    a = int(input("enter a number"))
+
+if a % 2 != 0:
+    print(a, "is not an even number.")
